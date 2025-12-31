183c9b923d58c81217bd953196c52cda7aeffeac
BL-subhapratha <subhapratha.sivaramakrishnan@bridgelabz.com>
2025-12-31 09:13:05 +0000

    UC2 - Render OOPS as Banner using Print Statements

---
+++ src/main/java/com/cg/UC2OOPSBannerApp.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+public class UC2OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("   ***   " + " "+ "   ***   " + " "+ "******   " + " "+ "   *****");
+        System.out.println(" **   ** " + " "+ " **   ** " + " "+ "**    ** " + " "+ "**");
+        System.out.println("**     **" + " "+ "**     **" + " "+ "**    ** " + " "+ " **");
+        System.out.println("**     **" + " "+ "**     **" + " "+ "******   " + " "+ "    ****");
+        System.out.println("**     **" + " "+ "**     **" + " "+ "**       " + " "+ "      **");
+        System.out.println(" **   ** " + " "+ " **   ** " + " "+ "**       " + " "+ "     **");
+        System.out.println("   ***   " + " "+ "   ***   " + " "+ "**       " + " "+ "*****");
+    }
+}
