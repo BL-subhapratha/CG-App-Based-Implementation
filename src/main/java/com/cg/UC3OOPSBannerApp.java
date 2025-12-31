195afa7696cf313c1ef25407e7bd19da70a2892a
BL-subhapratha <subhapratha.sivaramakrishnan@bridgelabz.com>
2025-12-31 09:32:03 +0000

    UC3 - Render OOPS as Banner using String.join()

---
+++ src/main/java/com/cg/UC3OOPSBannerApp.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+public class UC3OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println(String.join("  " , " ", "   ***   " , "  ***     ", "******   " , "   *****"));
+        System.out.println(String.join("  " , " ", " **   ** " , " **   **  ", "**    ** " , "**"));
+        System.out.println(String.join("  " , " ", "**     **" , "**     ** ", "**    ** " , " **"));
+        System.out.println(String.join("  " , " ", "**     **" , "**     ** ", "******   " , "    ****"));
+        System.out.println(String.join("  " , " ", "**     **" , "**     ** ", "**       " , "      **"));
+        System.out.println(String.join("  " , " ", " **   ** " , " **   **  ", "**       " , "     **"));
+        System.out.println(String.join("  " , " ", "   ***   " , "   ***    ", "**       " , "*****"));
+    }
+}
